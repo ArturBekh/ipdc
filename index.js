6e9a57592ed912e2b268a19d7dcb366e5e50d594
ArturBekh <85341278+ArturBekh@users.noreply.github.com>
2021-06-04 13:06:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+// $("h1").css("color","red");
+
+// $(document).ready(function(){
+$(".burger__menu").click(function(event){
+  $(".burger__menu,.nav-bar-content").toggleClass("active");
+});
+// })
